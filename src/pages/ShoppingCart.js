9d1cc86391e792844021593e98a4d428e8c77be9
ChaoChi <chaochicheng696@gmail.com>
2021-04-05 20:24:29 +0800

    first commit

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,28 @@
+import { Layout } from 'antd';
+import ShoppingDetail from "../components/ShoppingDetail.js";
+import NavBar from "../components/NavBar";
+import AppHeader from "../components/Header"
+import AppFooter from "../components/Footer"
+
+const { Header, Content, Footer } = Layout;
+
+function ShoppingCart() {
+  return (
+    <Layout className="container main-layout">
+      <NavBar />
+      <Layout className="bg-gray">     
+        <Header className="layout-header">
+          <AppHeader title="Shopping Cart" />
+        </Header>
+        <Content className="layout-content">
+          <ShoppingDetail/>
+        </Content>
+        <Footer className="layout-footer">
+           <AppFooter />
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default ShoppingCart;
